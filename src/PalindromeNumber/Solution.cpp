555e5a691640534a7f4f84eb65cbfd586dad3a26
Imtiaz Ahmed <imtiaz@imtiazahmed.dev>
2020-02-25 05:52:07 -0700

    fix: clang-format and source file

--- src/PalindromeNumber/Solution.cpp
+++ src/PalindromeNumber/Solution.cpp
@@ -7,62 +7,58 @@ using namespace std;
 
 class Solution {
 public:
-    bool isPalindrome(int x) {
-        if (x >= 0 && x < 10) {
-		return true;
-	}
-	else if (x >= 10) {
-		vector<int> vec;
-		long div = 10;
-		long ct = 1;
-		int num = 0;
-		int digCount = 0;
-		int temp = x;
+  bool isPalindrome(int x) {
+    if (x >= 0 && x < 10) {
+      return true;
+    } else if (x >= 10) {
+      vector<int> vec;
+      long div = 10;
+      long ct = 1;
+      int num = 0;
+      int digCount = 0;
+      int temp = x;
 
-		while (temp != 0) {
-			temp /= 10;
-			digCount++;
-		}
+      while (temp != 0) {
+        temp /= 10;
+        digCount++;
+      }
 
-		for (int i = 0; i < digCount; i++) {
-			num = x % div;
-			num /= ct;
-			div *= 10;
-			ct *= 10;
-			vec.push_back(num);
-		}
-		
-		for (int i = 0; i < vec.size(); i++) {
-			if (i == vec.size() - 1) return true;
-			else if (vec[i] != vec[vec.size() - 1 - i]) return false;
-		}
+      for (int i = 0; i < digCount; i++) {
+        num = x % div;
+        num /= ct;
+        div *= 10;
+        ct *= 10;
+        vec.push_back(num);
+      }
 
-	}
-	else {
-		return false;
-	}
-    
+      for (int i = 0; i < vec.size(); i++) {
+        if (i == vec.size() - 1)
+          return true;
+        else if (vec[i] != vec[vec.size() - 1 - i])
+          return false;
+      }
+
+    } else {
+      return false;
+    }
+
     return false;
-};
+  };
 };
 
-int stringToInteger(string input) {
-    return stoi(input);
-}
+int stringToInteger(string input) { return stoi(input); }
 
-string boolToString(bool input) {
-    return input ? "True" : "False";
-}
+string boolToString(bool input) { return input ? "True" : "False"; }
 
 int main() {
-    string line;
-    while (getline(cin, line)) {
-        int x = stringToInteger(line);
-        
-        bool ret = Solution().isPalindrome(x);
+  string line;
+  while (getline(cin, line)) {
+    int x = stringToInteger(line);
 
-        string out = boolToString(ret);
-        cout << out << endl;
-    }
-    return 0;
+    bool ret = Solution().isPalindrome(x);
+
+    string out = boolToString(ret);
+    cout << out << endl;
+  }
+  return 0;
 }
