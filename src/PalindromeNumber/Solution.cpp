4fd85b7be3036e057c631e84a9fa999f1609a6df
Imtiaz Ahmed <imtiaz@imtiazahmed.dev>
2020-02-25 15:23:28 -0700

    test: added test

--- src/PalindromeNumber/Solution.cpp
+++ src/PalindromeNumber/Solution.cpp
@@ -3,13 +3,16 @@
 #include <iostream>
 #include <vector>
 
+#include "../Test.h"
+
 using namespace std;
+using namespace leetcode;
 
 class Solution {
 public:
-  bool isPalindrome(int x) {
+  string isPalindrome(int x) {
     if (x >= 0 && x < 10) {
-      return true;
+      return "true";
     } else if (x >= 10) {
       vector<int> vec;
       long div = 10;
@@ -33,16 +36,16 @@ public:
 
       for (int i = 0; i < vec.size(); i++) {
         if (i == vec.size() - 1)
-          return true;
+          return "true";
         else if (vec[i] != vec[vec.size() - 1 - i])
-          return false;
+          return "false";
       }
 
     } else {
-      return false;
+      return "false";
     }
 
-    return false;
+    return "false";
   };
 };
 
@@ -51,14 +54,25 @@ int stringToInteger(string input) { return stoi(input); }
 string boolToString(bool input) { return input ? "True" : "False"; }
 
 int main() {
-  string line;
-  while (getline(cin, line)) {
-    int x = stringToInteger(line);
 
-    bool ret = Solution().isPalindrome(x);
+  Solution s;
 
-    string out = boolToString(ret);
-    cout << out << endl;
-  }
+  test("Test case: ", s.isPalindrome(121), "true");
+  test("Test case: ", s.isPalindrome(120), "true");
+  test("Test case: ", s.isPalindrome(-121), "true");
+  test("Test case: ", s.isPalindrome(11), "true");
+  test("Test case: ", s.isPalindrome(9), "true");
+  test("Test case: ", s.isPalindrome(0), "true");
+  test("Test case: ", s.isPalindrome(10022001), "true");
+  
+  // string line;
+  // while (getline(cin, line)) {
+  //   int x = stringToInteger(line);
+
+  //   bool ret = Solution().isPalindrome(x);
+
+  //   string out = boolToString(ret);
+  //   cout << out << endl;
+  // }
   return 0;
 }
